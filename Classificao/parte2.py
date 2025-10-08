978274fd98e2589f4dee1be939c6f78a43a222bb
guimoll <guilhermemollsilva@gmail.com>
2025-10-08 13:08:42 -0300

    necessario implementar as respostas e verificar o KNN como um todo

--- Classificao/parte2.py
+++ Classificao/parte2.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     testRots = mat['testRots'].flatten() #flatten pra ser uma matriz de 1 dimensão
     trainRots = mat['trainRots'].flatten()  # flatten pra ser uma matriz de 1 dimensão
 
-    rotuloPrevisto = np.array(KNN.meuKnn(grupoTrain, trainRots, grupoTest, 16))
+    rotuloPrevisto = np.array(KNN.meuKnn(grupoTrain, trainRots, grupoTest, 32, True))
     print("Predicted labels:", rotuloPrevisto)
 
     estaCorreto = rotuloPrevisto == testRots
@@ -30,14 +30,22 @@ if __name__ == '__main__':
 
 best_acc = 0
 best_k = 1
+k_98 = 0
+
 for k in range(1, 49):
-    rotuloPrevisto = KNN.np.array(KNN.meuKnn(grupoTrain, trainRots, grupoTest, k))
+    rotuloPrevisto = KNN.np.array(KNN.meuKnn(grupoTrain, trainRots, grupoTest, k, True))
     acuracia = np.sum(rotuloPrevisto == testRots) / len(testRots)
     if acuracia > best_acc:
         best_acc = acuracia
         best_k = k
 
+        # Para dar tolerancia
+    if abs(acuracia - 0.98) < 0.005:
+        k_98 = k
+
 print("Melhor acuracia:", best_acc)
 print("Melhor numero de vizinhos:", best_k)
+print("Acuracia 98% atingida com k =", k_98)
+
 
 #A acurácia inicial obtida foi de 78,3%, ao aplicar o mesmo kNN do primeiro experimento diretamente sobre os dados brutos.
